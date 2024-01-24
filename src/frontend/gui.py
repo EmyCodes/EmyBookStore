aad0524dd825d13500ba999d2db3ccb8fdd7bb6a
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 11:50:59 +0100

    feat: add stringvar() to each entry

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -34,16 +34,20 @@ l4 = Label(window, text="ISBN")
 l4.grid(row=1, column=2)
 
 # Entry
-e1 = Entry(window)
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
 e1.grid(row=0, column=1)
 
-e2 = Entry(window)
+author_text = StringVar()
+e2 = Entry(window, textvariable=author_text)
 e2.grid(row=0, column=3)
 
-e3 = Entry(window)
+year_text = StringVar()
+e3 = Entry(window, textvariable=year_text)
 e3.grid(row=1, column=1)
 
-e4 = Entry(window)
+isbn_text = StringVar()
+e4 = Entry(window, textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
 # Buttons
