70583277742bf988ff647d7b5028946181bd3cc1
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 09:57:13 +0100

    docs: update with a docstring

---
+++ src/frontend/gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+GUI of BookStore
+
+HAS ENTRY:
+    Title
+    Author
+    Year
+    ISBN
+
+HAS BUTTON;
+    View
+    Search Entry
+    Add Entry
+    Update Selected
+    Delete Selected
+    Close
+"""
