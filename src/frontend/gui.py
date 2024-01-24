b61c7594b3ee25e1dd233ffa7ad77324e9e8d095
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 10:12:11 +0100

    feat: entry

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -20,6 +20,7 @@ HAS BUTTON;
 
 window = Tk()
 
+# Labels
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
@@ -31,5 +32,19 @@ l3.grid(row=1, column=0)
 
 l4 = Label(window, text="ISBN")
 l4.grid(row=1, column=2)
+
+# Entry
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+e2 = Entry(window)
+e2.grid(row=0, column=3)
+
+e3 = Entry(window)
+e3.grid(row=1, column=1)
+
+e4 = Entry(window)
+e4.grid(row=1, column=3)
+
 
 window.mainloop()
