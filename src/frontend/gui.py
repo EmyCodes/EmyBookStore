212d1721691da8f837c9c64d7cf3807dc84f6e01
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 10:19:29 +0100

    feat: buttons

--- src/frontend/gui.py
+++ src/frontend/gui.py
@@ -46,5 +46,23 @@ e3.grid(row=1, column=1)
 e4 = Entry(window)
 e4.grid(row=1, column=3)
 
+# Buttons
+b1 = Button(window, text="View all")
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text="Search entry")
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text="Add entry")
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text="Update")
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text="Delete")
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text="Close")
+b6.grid(row=7, column=3)
 
 window.mainloop()
