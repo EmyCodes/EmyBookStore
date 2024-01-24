ee52a9b68b598850a025edf113723547f04d964d
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 10:38:23 +0100

    feat: create connection

---
+++ src/backend/engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sqlite3
+"""
+The Engineering Part of BookStore
+"""
+
+def connect():
+    conn = sqlite3.connect("BookStore.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS BookStore (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
+connect()
