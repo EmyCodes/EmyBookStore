ed5b2b8c4a5eae455eee6557905cee14e5b285c4
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-24 13:32:40 +0100

    version: version1

---
+++ v1/src/backend/engine.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+import sqlite3
+"""
+The Engineering Part of the BookStore
+"""
+
+def connect():
+    """
+    Function creates connection to the database
+    """
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS BookStore (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+
+def view():
+    """
+    Function list items of the database
+    """
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM BookStore")
+    rows = cur.fetchall()
+    conn.close()
+    for i in range(len(rows)):
+        print(rows[i])
+    return rows
+
+def search(title, author, year, isbn):
+    """
+    Searches for Specified iem
+    """
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM BookStore WHERE title=? OR author=? OR year=? OR isbn", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    for i in range(len(rows)):
+        print(rows[i])
+    return rows
+
+
+def add(title, author, year, isbn):
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO BookStore VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+
+def update(id, title="", author="", year="", isbn=""):
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE BookStore SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+
+def delete(id):
+    conn = sqlite3.connect("EmyBookStore.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM BookStore WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+def close():
+    pass
+
+
+connect()
+# view()
+# search("Believer's Authority", "Kenneth E. Hagin", 1980, 73773737373)
+# add("Believer's Authority", 'Kenneth E. Hagin', 1980, 73773737373)
+# add("Believer's Love Walk", 'Kenneth E. Hagin', 1983, 30736593)
+# add('The Anointing', 'Kenneth E. Hagin', 1960, 12455673)
+# add('Healing', 'Kenneth E. Hagin', 1965, 334567213)
+# add('Tongues Beyond Upper Room', 'Kenneth E. Hagin', 1995, 237213)
+# add('How To Be Led By The Spirit', 'Kenneth E. Hagin', 1960, 3445446)
+# delete(7)
+# close()
+# update(7, "The Triumphant Church", 'Kenneth E. Hagin', 1960, 3445446)
+view()
---
+++ v1/src/frontend/gui.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+from tkinter import *
+
+from backend.engine import connect, add, update, delete, search, view, close
+
+"""
+GUI of BookStore
+
+HAS ENTRY:
+    Title
+    Author
+    Year
+    ISBN
+
+HAS BUTTON;
+    View
+    Search Entry
+    Add Entry
+    Update Selected
+    Delete Selected
+    Close
+"""
+
+def view_all():
+    for row in view():
+        list_box.insert(END, row)
+
+
+window = Tk()
+
+
+# Labels
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+# Entry
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text, border=2)
+e1.grid(row=0, column=1)
+
+author_text = StringVar()
+e2 = Entry(window, textvariable=author_text, border=2)
+e2.grid(row=0, column=3)
+
+year_text = StringVar()
+e3 = Entry(window, textvariable=year_text, border=2)
+e3.grid(row=1, column=1)
+
+isbn_text = StringVar()
+e4 = Entry(window, textvariable=isbn_text, border=2)
+e4.grid(row=1, column=3)
+
+# Buttons
+b1 = Button(window, text="View all", width=12, border=3, command=view_all)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text="Search entry", width=12, border=3, command="")
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text="Add entry", width=12, border=3, command='')
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text="Update", width=12, border=3, command='')
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text="Delete", width=12, border=3, command='')
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text="Close", width=12, border=3)
+b6.grid(row=7, column=3)
+
+# ListBox
+list_box = Listbox(window, height=6, width=35, border=2)
+list_box.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+sb1 = Scrollbar(window, width=15)
+sb1.grid(row=2, column=2, rowspan=6)
+
+list_box.config(yscrollcommand=sb1.set)
+sb1.config(command=list_box.yview)
+
+window.mainloop()
