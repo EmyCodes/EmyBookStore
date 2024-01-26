2a81f3865880f8d3c88fb97da609cb97c43140aa
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-26 13:30:40 +0100

    style: pass pycodestyle

--- models/backend.py
+++ models/backend.py
@@ -15,9 +15,9 @@ class dbModel():
         """
         self.conn = sqlite3.connect(".EmyBookStore.db")
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS BookStore \
-                    (id INTEGER PRIMARY KEY, title text, \
-                    author text, year integer, isbn integer)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS BookStore "
+                         "(id INTEGER PRIMARY KEY, title text, "
+                    "author text, year integer, isbn integer)")
         self.conn.commit()
 
 
@@ -35,9 +35,7 @@ class dbModel():
         """
         self.cur.execute("SELECT * FROM BookStore")
         self.rows = self.cur.fetchall()
-        # self.conn.close()
-        # for i in range(len(rows)):
-        #     print(rows[i])
+        self.conn.close()
         return self.rows
 
 
@@ -45,26 +43,20 @@ class dbModel():
         """
         Method searches for Specified item
         """
-        self.cur.execute("SELECT * FROM BookStore WHERE title=? \
-                    OR author=? OR year=? OR isbn=?",
+        self.cur.execute("SELECT * FROM BookStore WHERE title=? "
+                    "OR author=? OR year=? OR isbn=?",
                     (title, author, year, isbn))
         self.rows = self.cur.fetchall()
         self.conn.close()
-        # if len(rows) == 0:
-        #     return "Not Found"
-        # print(rows)
-        # self.conn.close()
-        # for i in range(len(rows)):
-        #     print(rows[i])
         return self.rows
 
 
-    def add(self,title, author, year, isbn):
+    def add(self, title, author, year, isbn):
         """
         Method adds entry to the database
         """
-        self.cur.execute("INSERT INTO BookStore \
-                    VALUES (NULL, ?, ?, ?, ?)",
+        self.cur.execute("INSERT INTO BookStore "
+                    "VALUES (NULL, ?, ?, ?, ?)",
                     (title, author, year, isbn))
         self.conn.commit()
         self.conn.close()
@@ -74,8 +66,8 @@ class dbModel():
         """
         Method updates existing data
         """
-        self.cur.execute("UPDATE BookStore SET title=?, author=?, year=?, \
-                    isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.cur.execute("UPDATE BookStore SET title=?, author=?, year=?, "
+                    "isbn=? WHERE id=?", (title, author, year, isbn, id))
         self.conn.commit()
         self.conn.close()
 
@@ -90,4 +82,3 @@ class dbModel():
 
 
 # db = dbModel()
-# print(db.view())
