14ade163278194f8ce2b3990a1e1a60e4fa7d2a3
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2024-01-26 12:25:36 +0100

    feat: wrap into class and methods

--- src/backend.py
+++ src/backend.py
@@ -4,90 +4,75 @@ import sqlite3
 The Engineering Part of the BookStore
 """
 
-
-def connect():
+class dbModel():
     """
-    Function creates connection to the database
+    Class Connect and Perform defined Methods
     """
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS BookStore \
-                (id INTEGER PRIMARY KEY, title text, \
-                author text, year integer, isbn integer)")
-    conn.commit()
-    conn.close()
+    def __init__(self):
+        self.conn = sqlite3.connect(".EmyBookStore.db")
+        self.cur = self.conn.cursor()
 
 
-def view():
-    """
-    Function list items of the database
-    """
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM BookStore")
-    rows = cur.fetchall()
-    conn.close()
-    # for i in range(len(rows)):
-    #     print(rows[i])
-    return rows
+    def connect(self):
+        """
+        Function creates connection to the database
+        """
+        self.cur.execute("CREATE TABLE IF NOT EXISTS BookStore \
+                    (id INTEGER PRIMARY KEY, title text, \
+                    author text, year integer, isbn integer)")
+        self.conn.commit()
+        self.conn.close()
 
 
-def search(title="", author="", year="", isbn=""):
-    """
-    Searches for Specified iem
-    """
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM BookStore WHERE title=? \
-                OR author=? OR year=? OR isbn=?",
-                (title, author, year, isbn))
-    rows = cur.fetchall()
-    # if len(rows) == 0:
-    #     return "Not Found"
-    # print(rows)
-    conn.close()
-    # for i in range(len(rows)):
-    #     print(rows[i])
-    return rows
+    def view(self):
+        """
+        Function list items of the database
+        """
+        self.cur.execute("SELECT * FROM BookStore")
+        self.rows = self.cur.fetchall()
+        self.conn.close()
+        # for i in range(len(rows)):
+        #     print(rows[i])
+        return self.rows
 
 
-def add(title, author, year, isbn):
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO BookStore \
-                VALUES (NULL, ?, ?, ?, ?)",
-                (title, author, year, isbn))
-    conn.commit()
-    conn.close()
+    def search(self, title="", author="", year="", isbn=""):
+        """
+        Searches for Specified iem
+        """
+        self.cur.execute("SELECT * FROM BookStore WHERE title=? \
+                    OR author=? OR year=? OR isbn=?",
+                    (title, author, year, isbn))
+        self.rows = self.cur.fetchall()
+        # if len(rows) == 0:
+        #     return "Not Found"
+        # print(rows)
+        self.conn.close()
+        # for i in range(len(rows)):
+        #     print(rows[i])
+        return self.rows
 
 
-def update(id, title="", author="", year="", isbn=""):
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE BookStore SET title=?, author=?, year=?, \
-                isbn=? WHERE id=?", (title, author, year, isbn, id))
-    conn.commit()
-    conn.close()
+    def add(self,title, author, year, isbn):
+        self.cur.execute("INSERT INTO BookStore \
+                    VALUES (NULL, ?, ?, ?, ?)",
+                    (title, author, year, isbn))
+        self.conn.commit()
+        self.conn.close()
 
 
-def delete(id):
-    conn = sqlite3.connect(".EmyBookStore.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM BookStore WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
+    def update(self, id, title="", author="", year="", isbn=""):
+        self.cur.execute("UPDATE BookStore SET title=?, author=?, year=?, \
+                    isbn=? WHERE id=?", (title, author, year, isbn, id))
+        self.conn.commit()
+        self.conn.close()
 
 
-connect()
-# view()
-search(author="Kenneth E. Hagin")
-# add("Believer's Authority", 'Kenneth E. Hagin', 1980, 73773737373)
-# add("Believer's Love Walk", 'Kenneth E. Hagin', 1983, 30736593)
-# add('The Anointing', 'Kenneth E. Hagin', 1960, 12455673)
-# add('Healing', 'Kenneth E. Hagin', 1965, 334567213)
-# add('Tongues Beyond Upper Room', 'Kenneth E. Hagin', 1995, 237213)
-# add('How To Be Led By The Spirit', 'Kenneth E. Hagin', 1960, 3445446)
-# delete(7)
-# close()
-# update(7, "The Triumphant Church", 'Kenneth E. Hagin', 1960, 3445446)
-view()
+    def delete(self, id):
+        self.cur.execute("DELETE FROM BookStore WHERE id=?", (id,))
+        self.conn.commit()
+        self.conn.close()
+
+
+db = dbModel()
+# print(db.view())
